3e0ef67e99f617f91f2ff3e7841adbfc7a222386
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-03-16 12:29:15 +0700

    fix path for ssr

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react';
 import theme from './theme/theme';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <HashRouter basename="/portfolio">
+      {/* <BrowserRouter basename="/portfolio"> */}
+        <ChakraProvider theme={theme}>
+          <App />
+        </ChakraProvider>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
