7d6d86d63d207e1d25945d7f64c5968b6d65c8a3
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-07-14 16:29:22 +0700

    update projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,6 +2,7 @@ import Container from "layouts/Container";
 import React from "react";
 import Image from "next/image";
 import projects from "@/data/projects";
+import { LinkSimple, GithubLogo } from "phosphor-react";
 
 const Projects = () => {
     return (
@@ -23,102 +24,26 @@ const Projects = () => {
                             <div className="flex space-x-4 mb-4">
                                 {project.link && (
                                     <a
-                                        className="text-base decoration-neutral-500 text-neutral-700 hover:text-black"
+                                        className="text-base decoration-neutral-500 text-neutral-700 hover:text-black dark:text-slate-300 dark:decoration-slate-300 hover:dark:text-slate-200"
                                         href={project.link}
                                     >
                                         Preview
-                                        <svg
+                                        <LinkSimple
                                             className="inline ml-1"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="16"
-                                            height="16"
-                                            fill="#000000"
-                                            viewBox="0 0 256 256"
-                                        >
-                                            <rect
-                                                width="256"
-                                                height="256"
-                                                fill="none"
-                                            ></rect>
-                                            <path
-                                                d="M122.3,71.4l19.8-19.8a44.1,44.1,0,0,1,62.3,62.3l-28.3,28.2a43.9,43.9,0,0,1-62.2,0"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                            <path
-                                                d="M133.7,184.6l-19.8,19.8a44.1,44.1,0,0,1-62.3-62.3l28.3-28.2a43.9,43.9,0,0,1,62.2,0"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                        </svg>
+                                            size={18}
+                                        />
                                     </a>
                                 )}
                                 {project.githubLink && (
                                     <a
-                                        className="text-base decoration-neutral-500 text-neutral-700 hover:text-black"
+                                        className="text-base decoration-neutral-500 text-neutral-700 hover:text-black dark:text-slate-300 dark:decoration-slate-300 hover:dark:text-slate-200"
                                         href={project.githubLink}
                                     >
                                         Github
-                                        <svg
+                                        <GithubLogo
                                             className="inline ml-1"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="20"
-                                            height="20"
-                                            fill="#000000"
-                                            viewBox="0 0 256 256"
-                                        >
-                                            <rect
-                                                width="256"
-                                                height="256"
-                                                fill="none"
-                                            ></rect>
-                                            <path
-                                                d="M84,240a23.9,23.9,0,0,0,24-24V168"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                            <path
-                                                d="M172,240a23.9,23.9,0,0,1-24-24V168"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                            <path
-                                                d="M152,168h16a23.9,23.9,0,0,1,24,24v8a23.9,23.9,0,0,0,24,24"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                            <path
-                                                d="M104,168H88a23.9,23.9,0,0,0-24,24v8a23.9,23.9,0,0,1-24,24"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                            <path
-                                                d="M111.8,64A52,52,0,0,0,68,40a52,52,0,0,0-3.5,44.7A49.3,49.3,0,0,0,56,112v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.3,49.3,0,0,0-8.5-27.3A52,52,0,0,0,188,40a52,52,0,0,0-43.8,24Z"
-                                                fill="none"
-                                                stroke="#000000"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="16"
-                                            ></path>
-                                        </svg>
+                                            size={18}
+                                        />
                                     </a>
                                 )}
                             </div>
