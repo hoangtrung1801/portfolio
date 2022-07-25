5eed17da2d93bbffbe59a1cdd0ed15baa35a2440
Hoang Trung <hoangtrung1801.2003@gmail.com>
2022-07-25 17:11:23 +0700

    update domain

--- components/CustomNProgress.tsx
+++ components/CustomNProgress.tsx
@@ -2,7 +2,6 @@ import NextNProgress from "nextjs-progressbar";
 import { theme } from "tailwind.config";
 
 const CustomNProgress = () => {
-    console.log(theme.extend.colors.teal);
     return <NextNProgress color={theme.extend.colors.teal[500]} />;
 };
 
